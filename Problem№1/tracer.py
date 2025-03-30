b61dfc820795551cb74d278b7b9c067de120ddb9
xebuk <sozdykov2017@yandex.ru>
2025-03-31 02:02:55 +0500

    Made first task

--- Problem№1/tracer.py
+++ Problem№1/tracer.py
@@ -1,24 +1,34 @@
 import re
 import subprocess
-import ipaddress
+import argparse
 from ipwhois import IPWhois
 from ipwhois.exceptions import IPDefinedError, ASNRegistryError
 
 
-def trace_route(target):
+MAX_TIMEOUT = 150
+
+
+def trace_route(addr: str) -> list[str]:
     try:
         result = subprocess.run(
-            f"tracert -d {target}",
+            f"tracert -d -h 50 {addr}",
             capture_output=True,
             text=True,
             encoding='cp866',
+            timeout=MAX_TIMEOUT,
             check=True
         )
     except subprocess.CalledProcessError as e:
         print(f"Ошибка трассировки:\n{e.stderr}")
-        return []
+        exit(0)
+    except subprocess.TimeoutExpired:
+        print("Превышено время ожидания.")
+        exit(0)
+    except subprocess.SubprocessError as e:
+        print(f"Ошибка подпроцесса:\n{str(e)}")
+        exit(0)
 
-    ips = []
+    ips = list()
     for line in result.stdout.splitlines():
         ip = extract_ip(line)
         if ip:
@@ -31,18 +41,10 @@ def extract_ip(line):
     return matches[-1] if matches else None
 
 
-def is_public_ip(ip_str):
-    try:
-        ip = ipaddress.ip_address(ip_str)
-        return not (ip.is_private or ip.is_loopback or ip.is_link_local)
-    except ValueError:
-        return False
-
-
 def get_asn_info(ip):
     try:
-        obj = IPWhois(ip)
-        result = obj.lookup_rdap()
+        ipwhois_instance = IPWhois(ip)
+        result = ipwhois_instance.lookup_rdap()
 
         provider = "Неизвестно"
         if 'entities' in result and result['entities']:
@@ -55,7 +57,7 @@ def get_asn_info(ip):
         }
 
     except IPDefinedError:
-        return {'error': 'Приватный IP'}
+        return {'error': 'Приватный IP или локальный адрес'}
     except (ASNRegistryError, KeyError):
         return {'error': 'Данные не найдены'}
     except Exception as e:
@@ -63,28 +65,35 @@ def get_asn_info(ip):
 
 
 def main():
-    target = input("Введите домен или IP адрес: ").strip()
-    print(f"\nТрассировка до {target}...")
-    ips = trace_route(target)
+    parser = argparse.ArgumentParser(
+        description="Трассировщик автономных систем - осуществляет трассировку до указанного узла и для каждого из посещенных узлов выводит его IP-адрес, номер автономной системы, страну и провайдера.",
+        epilog="Для работы требуется установленный модуль ipwhois (pip install ipwhois)"
+    )
+    parser.add_argument(
+        dest="addr",
+        help="IP-адрес или доменное имя, до которого будет выполняться трассировка"
+    )
+
+    args = parser.parse_args()
+
+    print(f"Трассировка до {args.addr}...")
+    ips = trace_route(args.addr)
 
     if not ips:
         print("Маршрут не найден или цель недостижима")
         return
 
     print("\nРезультат трассировки:")
-    print(f"{'Хоп':<5}{'IP':<16}{'ASN':<10}{'Страна':<8}Провайдер")
+    print(f"{'Хоп':<5}{'IP':<16}{'ASN':<10}{'Страна':<8}{'Провайдер':<21}")
     print("-" * 60)
 
-    for hop, ip in enumerate(ips, 1):
-        if is_public_ip(ip):
-            info = get_asn_info(ip)
-            if 'error' in info:
-                status = info['error']
-                print(f"{hop:<5}{ip:<16}{status:<10}")
-            else:
-                print(f"{hop:<5}{ip:<16}{info['asn']:<10}{info['country']:<8}{info['provider']}")
+    for hop, ip in enumerate(ips[1:], 1):
+        info = get_asn_info(ip)
+        if 'error' in info:
+            status = info['error']
+            print(f"{hop:<5}{ip:<16}{status:<10}")
         else:
-            print(f"{hop:<5}{ip:<16}{'Локальный адрес':<10}")
+            print(f"{hop:<5}{ip:<16}{info['asn']:<10}{info['country']:<8}{info['provider']}")
 
 
 if __name__ == "__main__":
